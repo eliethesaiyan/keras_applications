efafece4d4ad04d730bdc0251aea8ad8d84be8ac
Elie Magambo <eliemagambo@gmail.com>
2020-08-28 20:28:45 +0900

    Added dropout

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import tensorflow as tf
-from models.toy_model import make_toy_model
+import matplotlib.pyplot as plt
+from models.toy_model import make_sequential_model
+from models.toy_model import make_functional_model
+
 
 
 
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+X_train, X_test = X_train/255.0, X_test/255.0
+#y_train = tf.keras.utils.to_categorical(y_train, dtype='float32')
+#y_test = tf.keras.utils.to_categorical(y_test, dtype='float32')
 
-filters = 64
-kernel = (3,3)
-strides = 2
-model = make_toy_model(filters=filters, kernel=kernel, strides=strides)
-model.compile(optimizer='sgd', loss='mse')
-model.fit((X_train, y_train))
+print('shapes')
+print(X_train.shape)
+print(y_train[:5])
+filters = 32
+kernel = (3, 3)
+# model = make_sequential_model(filters=filters, kernel=kernel, strides=1)
+# model = make_sequential_model()
+model = make_functional_model()
+model.summary()
+model.compile(optimizer='sgd',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics='accuracy')
+history = model.fit(X_train, y_train, epochs=30, batch_size=32,
+                    validation_split=0.25)
+eval_score = model.evaluate(X_test, y_test)
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+print('evaluation score %.2f', eval_score)
+predict_score = model.predict(X_test[:10])
+print('{}, {}, {}, {}, {}, {},{},{}, {}, {}'.format(*[class_names[i] for i in
+                                           tf.keras.backend.argmax(predict_score)]))
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
--- models/toy_model.py
+++ models/toy_model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import activations
-from tensorflow.keras import Model
+from tensorflow.keras import models
 
 
 def _conv_block(inputs, filters, kernel, strides):
@@ -14,17 +14,40 @@ def _conv_block(inputs, filters, kernel, strides):
     x = layers.Conv2D(filters, kernel_size=kernel, strides=strides)(inputs)
     x = layers.BatchNormalization()(x)
     x = activations.relu(x)
-
-    return x
-
-
-def make_toy_model(filters, kernel, strides ):
-
-    inputs = layers.Input(shape=(None, None, 3))
-    x = _conv_block(inputs, filters, kernel, strides)
     output = layers.Dense(10, activation='relu')(x)
-    model = Model(inputs, output)
+
+    return output
+
+
+def make_functional_model():
+
+    inputs = tf.keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, (3, 3), activation='relu')(inputs)
+    x = layers.MaxPooling2D((2, 2))(x)
+    x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+    x = layers.MaxPooling2D((2, 2))(x)
+    x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dropout(rate=0.3)(x)
+    output = layers.Dense(10)(x)
+    model = models.Model(inputs=inputs, outputs=output)
+    model.build(input_shape=(32, 32, 3))
 
     return model
 
 
+def make_sequential_model():
+
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
+
+    return model
